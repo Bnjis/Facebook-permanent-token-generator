184c841d45af64851f76dbf89862f3933b0edaae
Benji <Benjamin.lienart@gmail.com>
2019-03-14 13:13:55 +0100

    Updates

---
+++ precache-manifest.30773cd704ddcb3caea054ef88a12a75.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9684d404b772597a801d",
+    "url": "/Facebook-permanent-token-generator/static/js/runtime~main.6ff47466.js"
+  },
+  {
+    "revision": "c6985d09984d198aa08f",
+    "url": "/Facebook-permanent-token-generator/static/js/main.139d62e9.chunk.js"
+  },
+  {
+    "revision": "d5575cf4ac9e2cd4b846",
+    "url": "/Facebook-permanent-token-generator/static/js/2.7ecbdfed.chunk.js"
+  },
+  {
+    "revision": "c6985d09984d198aa08f",
+    "url": "/Facebook-permanent-token-generator/static/css/main.b584e2dc.chunk.css"
+  },
+  {
+    "revision": "4e26dcd9e9262e743b856a7e94a5700c",
+    "url": "/Facebook-permanent-token-generator/index.html"
+  }
+];
